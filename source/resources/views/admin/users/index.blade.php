bcdcd3a27deda41bafc33cab48ee248f191714d6
Techraav <robin.marechal-2@etu.univ-tours.fr>
2016-02-15 16:13:06 +0100

    user list

--- source/resources/views/admin/users/index.blade.php
+++ source/resources/views/admin/users/index.blade.php
@@ -13,19 +13,21 @@
 	<table class="table table-striped table-hover">
 		<thead>
 			<tr>
-				<th>Nom Prénom</th>
-				<th>Adresse e-mail</th>
-				<th>Membre depuis le :</th>
-				<th>Level</th>
+				<th width="300">Nom Prénom</th>
+				<th width="400">Adresse e-mail</th>
+				<th width="150">Département</th>
+				<th width="150">Membre depuis le :</th>
+				<th width="50" align="center">Level</th>
 			</tr>
 		</thead>
 		<tbody>
 			@foreach ($users as $u)
 				<tr>
-					<td>{{ ucfirst($u->last_name) }} {{ucfirst($u->first_name) }}</td>
+					<td>{!! printUserLink($u->id) !!}</td>
 					<td>{{ $u->email }}</td>
-					<td>{{ $u->created_at }}</td>
-					<td>{{ $u->level }}</td>
+					<td><a href="{{ url('admin/departments/'.$u->department_id.'/members') }}">{{ App\Department::where('id', $u->department_id)->first()->name }} ({{ App\Department::where('id', $u->department_id)->first()->short_name }})</a></td>
+					<td>&nbsp; &nbsp; &nbsp; {{ date_format($u->created_at, 'j M Y') }}</td>
+					<td align="center">	<a href="{{ url('admin/levels/'.App\Level::where('level', $u->level)->first()->name.'/members') }}">{{ $u->level }}</a></td>
 				</tr>
 			@endforeach
 			
--- source/resources/views/layouts/app.blade.php
+++ source/resources/views/layouts/app.blade.php
@@ -67,14 +67,14 @@
     </nav>
     <br /> 
 <div class="row">
-  <div class="col-md-9">
+  <div class="col-lg-9">
   <div class="container">
     @include('flash::message')
     @yield('content')
   </div>
   </div>
     {{-- SIDEBAR --}}
-    <div class="col-md-3">
+    <div class="col-lg-3">
         <div class="panel panel-default">
             <div class="panel-heading">
                 Welcome message
